6aaf708883e3ee07ae0675acc3f4acbf96b27e10
alexacost <79877564+alexacost@users.noreply.github.com>
2021-04-27 01:24:11 -0300

    Arreglo Modal

--- index.php
+++ index.php
@@ -262,6 +262,7 @@ session_start();
     <script>
         window.jQuery || document.write('<script src="js/jquery-slim.min.js"><\/script>')
     </script>
+    <script src="js/jquery-slim.min.js" ></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
